048beb5d169bc434dfa19420571729e30b647f62
Jean-Francis Bastien <bhacaz@gmail.com>
2020-07-22 09:23:10 -0400

    Add Inflector::deep_transform_keys_in_object! and apply to entity

--- lib/bright_serializer/inflector.rb
+++ lib/bright_serializer/inflector.rb
@@ -59,5 +59,21 @@ class Inflector
         end
       end
     end
+
+    # File active_support/core_ext/hash/keys.rb, line 156
+    def deep_transform_keys_in_object!(object, &block)
+      case object
+      when Hash
+        object.keys.each do |key| # rubocop:disable Style/HashEachMethods
+          value = object.delete(key)
+          object[yield(key)] = deep_transform_keys_in_object!(value, &block)
+        end
+        object
+      when Array
+        object.map! { |e| deep_transform_keys_in_object!(e, &block) }
+      else
+        object
+      end
+    end
   end
 end
