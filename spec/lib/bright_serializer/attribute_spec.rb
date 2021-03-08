petalmd/bright_serializer

commit 63ef8cbe18cb8de5973b958bac8b7f6b59f3a78f

Try symbol and string keys when the object to serialize is an Hash (#54)

lib/bright_serializer/attribute.rb
@@ -21,7 +21,7 @@ module BrightSerializer
         if @block
           @block.arity.abs == 1 ? object.instance_eval(&@block) : object.instance_exec(object, params, &@block)
         elsif object.is_a?(Hash)
-          object[key]
+          object[key] || object[key.to_s]
         else
           object.send(key)
         end

.rubocop.yml
@@ -34,3 +34,6 @@ Metrics/CyclomaticComplexity:
 
 Metrics/AbcSize:
   Enabled: false
+
+RSpec/NestedGroups:
+  Enabled: false

spec/lib/bright_serializer/serializer_spec.rb
@@ -80,6 +80,33 @@ RSpec.describe BrightSerializer::Serializer do
       end
 
       it { expect(instance.to_hash).to eq(result) }
+
+      context 'when the object is an Hash with string keys' do
+        let(:serializer_class) do
+          Class.new do
+            include BrightSerializer::Serializer
+            attributes :first_name, :last_name
+          end
+        end
+
+        let(:user_hash) do
+          {
+            'first_name' => user.first_name,
+            'last_name' => user.last_name
+          }
+        end
+
+        let(:result) do
+          {
+            first_name: user.first_name,
+            last_name: user.last_name
+          }
+        end
+
+        it 'serialize all 3 attributes' do
+          expect(serializer_class.new(user_hash).to_hash).to eq(result)
+        end
+      end
     end
   end
 

commit 69c89ae195ee56a15413b6255aa722ce81986d43

When serializing an Hash, check present of the key before trying string
Fix regression introduced by #54

lib/bright_serializer/attribute.rb
@@ -21,7 +21,7 @@ module BrightSerializer
         if @block
           @block.arity.abs == 1 ? object.instance_eval(&@block) : object.instance_exec(object, params, &@block)
         elsif object.is_a?(Hash)
-          object[key] || object[key.to_s]
+          object.key?(key) ? object[key] : object[key.to_s]
         else
           object.send(key)
         end

spec/lib/bright_serializer/attribute_spec.rb
@@ -1,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative '../../share/user'
+
+RSpec.describe BrightSerializer::Attribute do
+  let(:object_to_serialize) { { first_name: Faker::Name.first_name, is_admin: false } }
+
+  describe '#serialize' do
+    context 'when object is an hash' do
+      shared_examples 'serializing_hash' do
+        it 'return the value' do
+          object_to_serialize.each_key do |key|
+            instance = described_class.new(key.to_sym, nil, nil)
+            expect(instance.serialize(object_to_serialize, {})).to eq object_to_serialize[key]
+          end
+        end
+      end
+
+      context 'when keys are symbol' do
+        it_behaves_like 'serializing_hash'
+      end
+
+      context 'when keys are string' do
+        let(:object_to_serialize) { { 'first_name' => Faker::Name.first_name, 'is_admin' => false } }
+
+        it_behaves_like 'serializing_hash'
+      end
+    end
+
+    context 'when object is an object' do
+      let(:object_to_serialize) do
+        User.new.tap { |user| allow(user).to receive(:is_admin).and_return(false) }
+      end
+
+      it 'return the value' do
+        %i[first_name last_name is_admin].each do |attribute|
+          instance = described_class.new(attribute, nil, nil)
+          expect(instance.serialize(object_to_serialize, {})).to eq object_to_serialize.send(attribute)
+        end
+      end
+    end
+  end
+end