d248c4f685ea18c9b0d1814ffe01559c44a8a317
Eric Proulx <eproulx@petalmd.com>
2022-01-21 02:00:44 +0100

    Fix frozen array in enum (#83)
    Git error: command `git rev-parse --verify branches` in directory /Users/eproulx/code/bright_serializer has failed.
    Revision branches does not exist in the repository https://github.com/Bhacaz/coveralls-ruby-reborn. Maybe you misspelled it?
    If this error persists you could try removing the cache directory '/Users/eproulx/.rbenv/versions/2.7.4/lib/ruby/gems/2.7.0/cache/bundler/git/coveralls-ruby-reborn-9a467814f734037aff078aa7ab9adc6150cae53a'
    e
    
    * Change deep_transform_keys_in_object! to deep_transform_keys_in_object
    Add spec when frozen array
    Add spec expecting camel casing
    
    * Remove out off scope changes
    
    * Use same implementation as active support
    
    https://github.com/rails/rails/blob/f95c0b7e96eb36bc3efc0c5beffbb9e84ea664e4/activesupport/lib/active_support/core_ext/hash/keys.rb#L116
    
    * Fix spec update changelog

--- lib/bright_serializer/inflector.rb
+++ lib/bright_serializer/inflector.rb
@@ -60,17 +60,15 @@ class Inflector
       end
     end
 
-    # File active_support/core_ext/hash/keys.rb, line 156
-    def deep_transform_keys_in_object!(object, &block)
+    # File active_support/core_ext/hash/keys.rb, line 116
+    def deep_transform_keys_in_object(object, &block)
       case object
       when Hash
-        object.keys.each do |key| # rubocop:disable Style/HashEachMethods
-          value = object.delete(key)
-          object[yield(key)] = deep_transform_keys_in_object!(value, &block)
+        object.each_with_object({}) do |(key, value), result|
+          result[yield(key)] = deep_transform_keys_in_object(value, &block)
         end
-        object
       when Array
-        object.map! { |e| deep_transform_keys_in_object!(e, &block) }
+        object.map { |e| deep_transform_keys_in_object(e, &block) }
       else
         object
       end
